8840e071fbc3221c158ff3647e4d45f7e61ededd
anniesteenson <anniesteenso@gmail.com>
2017-08-21 14:13:01 -0500

    Updated gitignore. Updated webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,39 +1,50 @@
-var path = require('path'),
-    paths = {
-        entry: path.join(__dirname, '..', 'client', 'router.js'),
-        build: path.join(__dirname, '..', 'public'),
-        output: path.join(__dirname, '..', 'public', 'bundle.js')
-    };
+const path = require('path'),
+	webpack = require('webpack'),
+	HTMLWebpackPlugin = require('html-webpack-plugin'), // Useful when bundles file names change dynamically
+	paths = {
+	    entry: path.join(__dirname, '..', 'client', 'router.js'),
+	    build: path.join(__dirname, '..', 'public'),
+	    output: path.join(__dirname, '..', 'public', 'bundle.js')
+	};
 
 module.exports = {
-    entry: [
-        paths.entry
-    ],
-    output: {
-        path: __dirname,
-        filename: paths.output,
+	devtool: 'eval-source-map', // Faster rebuild, dev env only
+  entry: [
+  	'webpack-hot-middleware/client?reload=true',
+	  paths.entry
+  ],
+  output: {
+    path: __dirname,
+    filename: paths.output,
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+        AliasName: 'alias-value'
     },
-    resolve: {
-        root: __dirname,
-        alias: {
-            AliasName: 'alias-value'
-        },
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/
-            },
-            {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    }
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+	      test: /\.jsx?$/,
+	      exclude: /(node_modules|bower_components)/,
+	      loader: 'babel-loader'
+      },
+	    {
+	    	test: /\.json?$/,
+		    loader: 'json-loader'
+	    },
+	    {
+	    	test: /\.css$/,
+		    loader: ['style-loader', 'css-loader']
+	    },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
 
 
