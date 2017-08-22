c5b6d2f4ddb81bf29ba88d289c059a7eba42b113
Annie Steenson <anniesteenso@gmail.com>
2017-08-21 19:34:16 -0500

    Removed api code and moved it to another repo

--- server/api-router.js
+++
@@ -1,8 +0,0 @@
-module.exports = apiRouter => {
-
-	apiRouter.route('/').get((req, res) => {
-		res.render('index.html');
-	});
-
-	return apiRouter;
-};
--- server/app.js
+++ server/app.js
@@ -1,21 +1,13 @@
 const path = require('path'),
-    express = require('express'),
-    app = express(),
-    apiRouter = require('./api-router')(express.Router()),
-	  bodyParser = require('body-parser');
+  app = require('express')();
 
 app.use(express.static(path.resolve('public')));
 app.set('view engine', 'html');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use('/api', apiRouter);
-
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve('public', 'index.html'));
+	res.sendFile(path.resolve('public', 'index.html'));
 });
 
 app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+	console.log('Express server is up on port 3000');
 });
