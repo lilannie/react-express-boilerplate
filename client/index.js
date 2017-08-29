398ce8bdc2700802357b131a633c53bfe5b4e53f
Annie Steenson <anniesteenso@gmail.com>
2017-08-29 13:46:17 -0500

    Added redux middleware

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logger from 'redux-logger';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 import reducers from './redux/reducers';
 import App from './containers/App';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(logger));
 
 ReactDOM.render(
 	<Provider store={store}>
